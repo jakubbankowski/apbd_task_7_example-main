117fdd36c2f2cd4ae74a162a19686e57cfcc6588
JakubBankowski <s33203@pjwstk.edu.pl>
2026-05-25 19:37:44 +0200

    Separated DTOs and finished 2 more commands

--- Controllers/PCsController.cs
+++ Controllers/PCsController.cs
@@ -1,4 +1,5 @@
 ﻿using APBD_TASK_7.Data;
+using APBD_TASK_7.DTOs;
 using APBD_TASK_7.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -17,10 +18,10 @@ public class PCsController : ControllerBase
     }
 
     [HttpGet]
-    public async Task<ActionResult<List<PCResponse>>> GetPCs()
+    public async Task<ActionResult<List<PCResponseDTO>>> GetPCs()
     {
         var pcs = await _context.Pcs
-            .Select(p => new PCResponse
+            .Select(p => new PCResponseDTO
             {
                 Id = p.Id,
                 Name = p.Name,
@@ -32,14 +33,37 @@ public class PCsController : ControllerBase
             .ToListAsync();
         return Ok(pcs);
     }
-}
 
-public class PCResponse
-{
-    public int Id { get; set; }
-    public string Name { get; set; } = string.Empty;
-    public double Weight { get; set; }
-    public int Warranty { get; set; }
-    public DateTime CreatedAt { get; set; }
-    public int Stock { get; set; }
+    [HttpGet("{id}")]
+    public async Task<ActionResult<PCResponseDTO>> GetPC(int id)
+    {
+        var pc = await _context.Pcs.Select(
+            p => new PCResponseDTO
+            {
+                Id = p.Id,
+                Name = p.Name,
+                Weight = p.Weight,
+                Warranty = p.Warranty,
+                CreatedAt = p.CreatedAt,
+                Stock = p.Stock
+            }).Where(p => p.Id == id).FirstOrDefaultAsync();
+        return Ok(pc);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<PC>> CreatePC(CreatePCRequestDTO request)
+    {
+        var pc = new PC
+        {
+            Name = request.Name,
+            Weight = request.Weight,
+            Warranty = request.Warranty,
+            CreatedAt = request.CreatedAt,
+            Stock = request.Stock,
+        };
+        _context.Pcs.Add(pc);
+        await _context.SaveChangesAsync();
+        
+        return CreatedAtAction(nameof(GetPCs), new { id = pc.Id }, pc);
+    }
 }
---
+++ DTOs/CreatePCRequestDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace APBD_TASK_7.Data;
+
+public class CreatePCRequestDTO
+{
+    public string Name { get; set; } = string.Empty;
+    public double Weight { get; set; }
+    public int Warranty { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public int Stock { get; set; }
+}
